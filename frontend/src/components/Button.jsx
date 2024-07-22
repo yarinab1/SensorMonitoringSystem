ec6ceee721554859a3af4b23d5239456b1a2ff07
yarinab1 <yarinaf1@gmail.com>
2024-07-22 23:26:00 +0300

    change buttons and background colors

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,5 +1,12 @@
-const Button = ({title, onClick}) => {
-    return <button className='shadow-2xl rounded-xl bg-white p-2 hover:bg-slate-300' onClick={onClick}>{title}</button>
-}
+const Button = ({ title, onClick }) => {
+    return (
+        <button
+            className='shadow-2xl rounded-xl bg-blue-500 text-white p-2 hover:bg-blue-600'
+            onClick={onClick}
+        >
+            {title}
+        </button>
+    );
+};
 
 export default Button;
